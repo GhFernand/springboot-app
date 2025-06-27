f3e80369ee6c4e96daa84cb32b547ae8b01a4410
Guilherme Fernando Garcia de Souza <c010925@Manfrim.scp.br>
2025-06-27 17:54:06 -0300

    Corrige start.sh para executar aplicação em background

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "[start.sh] Rodando script no diretório: $(pwd)" >> /tmp/middleware.log
-echo "[start.sh] Iniciando aplicação..." >> /tmp/middleware.log
-nohup java -jar target/integration-0.0.1-SNAPSHOT.jar >> /tmp/middleware.log 2>&1 &
+echo "[start.sh] Rodando script no diretório: $(pwd)"
+echo "[start.sh] Iniciando aplicação..."
+nohup java -jar target/integration-0.0.1-SNAPSHOT.jar > /tmp/middleware.log 2>&1 &
